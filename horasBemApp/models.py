4aea90b8be32f21dfbde6c6e46a52b29b21f482f
Lucas Dauto <lucasdauto79@gmail.com>
2019-05-18 14:33:09 -0300

    Criar model

--- horasBemApp/models.py
+++ horasBemApp/models.py
@@ -1,5 +1,34 @@
 from django.db import models
 
+estado_opc = [
+    ('AC', 'Acre'), 
+    ('AL', 'Alagoas'),
+    ('AP', 'Amapá'),
+    ('BA', 'Bahia'),
+    ('CE', 'Ceará'),
+    ('DF', 'Distrito Federal'),
+    ('ES', 'Espírito Santo'),
+    ('GO', 'Goiás'),
+    ('MA', 'Maranão'),
+    ('MG', 'Minas Gerais'),
+    ('MS', 'Mato Grosso do Sul'),
+    ('MT', 'Mato Grosso'),
+    ('PA', 'Pará'),
+    ('PB', 'Paraíba'),
+    ('PE', 'Pernanbuco'),
+    ('PI', 'Piauí'),
+    ('PR', 'Paraná'),
+    ('RJ', 'Rio de Janeiro'),
+    ('RN', 'Rio Grande do Norte'),
+    ('RO', 'Rondônia'),
+    ('RR', 'Roraima'),
+    ('RS', 'Rio Grande do Sul'),
+    ('SC', 'Santa Catarina'),
+    ('SE', 'Sergipe'),
+    ('SP', 'São Paulo'),
+    ('TO', 'Tocantins')
+]
+
 # Create your models here.
 class FaleAqui(models.Model):
     nome = models.CharField(max_length=100)
@@ -22,4 +51,29 @@ class aluno(models.Model):
     rg = models.CharField(max_length=10)
     dt_Nascimento = models.DateField()
     email = models.EmailField()
+    faculdade = models.ForeignKey(Faculdade, on_delete=models.CASCADE)
+    rua = models.CharField(max_length=140)
+    numero = models.IntegerField()
+    cep = models.CharField(max_length=10)
+    bairro = models.CharField(max_length=45)
+    cidade = models.CharField(max_length=45)
+    estado = models.CharField(max_length=2, choices=estado_opc)
+    foto = models.ImageField(upload_to='')
 
+    def __str__(self):
+        return self.nome
+
+class ong(models.Model):
+    nome = models.CharField(max_length=45)
+    nome_social = models.CharField(max_length=45)
+    sigla = models.CharField(max_length=6)
+    rua = models.CharField(max_length=140)
+    numero = models.IntegerField()
+    cep = models.CharField(max_length=10)
+    bairro = models.CharField(max_length=45)
+    cidade = models.CharField(max_length=45)
+    estado = models.CharField(max_length=2, choices=estado_opc)
+    foto = models.ImageField(upload_to='')
+
+    def __str__(self):
+        return self.nome_social
