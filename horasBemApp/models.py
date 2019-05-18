040b35eb0fbc1cf46981a51c40f89ad2d3c17a84
Lucas Dauto <lucasdauto79@gmail.com>
2019-05-18 01:56:28 -0300

    Models Faculdade e Aluno

--- horasBemApp/models.py
+++ horasBemApp/models.py
@@ -9,3 +9,17 @@ class FaleAqui(models.Model):
 
     def __str__(self):
         return "E-mail: "+ self.email + " | Assunto: "+ self.assunto
+
+class Faculdade(models.Model):
+    nome = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.nome
+
+class aluno(models.Model):
+    nome = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=12)
+    rg = models.CharField(max_length=10)
+    dt_Nascimento = models.DateField()
+    email = models.EmailField()
+
