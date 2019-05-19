66e1133cc75693a08d94083f2a0f2c3b7d999af7
Lucas Dauto <lucasdauto79@gmail.com>
2019-05-19 20:38:21 -0300

    Correção arquivo form

--- horasBemApp/admin.py
+++ horasBemApp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from horasBemApp.models import FaleAqui, Aluno, Faculdade, Ong
+from horasBemApp.models import FaleAqui, Aluno, Instituicao, Ong
 
 admin.site.site_title = "Admin Horas do bem"
 admin.site.site_header = "Administração Horas do bem"
